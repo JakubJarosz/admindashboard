b23930bb259e923c094a23cd290a12a006b0492a
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-03-07 13:33:35 +0100

    ad

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "components/Layout/Layout";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-     
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
