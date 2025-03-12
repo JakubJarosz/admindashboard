27417d7f69eb0110079a9155d5c418d5aa97800f
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-03-12 13:51:48 +0100

    add

--- client/src/pages/geography/Geography.jsx
+++ client/src/pages/geography/Geography.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Box, useTheme } from "@mui/material";
-import { useGetGeographyQuery } from "state/api";
-import Header from "components/Header";
-import { ResponsiveChoropleth } from "@nivo/geo";
-import { geoData } from "state/geoData";
+import { Box } from "@mui/material";
+// import { useGetGeographyQuery } from "state/api";
+// import Header from "components/Header";
+// import { ResponsiveChoropleth } from "@nivo/geo";
+// import { geoData } from "state/geoData";
 
 const Geography = () => {
-  const theme = useTheme();
-  const { data } = useGetGeographyQuery();
+  // const theme = useTheme();
+  // const { data } = useGetGeographyQuery();
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
+      Geography    
+      {/* <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
       <Box
       
         mt="40px"
@@ -95,7 +96,7 @@ const Geography = () => {
         ) : (
           <>Loading...</>
         )}
-      </Box>
+      </Box> */}
     </Box>
   );
 };
